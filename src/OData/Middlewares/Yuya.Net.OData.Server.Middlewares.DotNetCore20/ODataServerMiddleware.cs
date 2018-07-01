3cf8eca572231aab86c758c078d4882926db0576
Alper Konuralp <alper@konuralp.gen.tr>
2018-07-01 20:54:33 +0300

    OData için projeleri açtım.

---
+++ src/OData/Middlewares/Yuya.Net.OData.Server.Middlewares.DotNetCore20/ODataServerMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yuya.Net.OData.Server.DotNetCore20
+{
+    public class ODataServerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ODataServerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            Console.WriteLine($"Request for {httpContext.Request.Path} received ({httpContext.Request.ContentLength ?? 0} bytes)");
+
+            // Call the next middleware delegate in the pipeline 
+            await _next.Invoke(httpContext);
+        }
+
+    }
+}
---
+++ src/OData/Yuya.Net.OData.Server/IODataService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yuya.Net.OData.Server
+{
+    /// <summary>
+    /// OData Service Interface
+    /// </summary>
+    public interface IODataService
+    {
+    }
+}
